5d82017088898528aa0885f4a56ccff56ed54cb4
ankitamaru <ankitamaru90@gmail.com>
2023-12-13 14:14:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,47 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React, { useEffect, useState } from "react";
+import "./Components/style.css";
+import Header from "./Components/Header";
+import Form from "./Components/Form";
+import TodosList from "./Components/TodosList";
+
+const App = () => {
+
+  const initialState = JSON.parse(localStorage.getItem("todos")) || [];
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState(initialState);
+  const [editTodo, setEditTodo] = useState(null);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
+  return (
+    <div className="container">
+      <div className="app-wrapper">
+        <div>
+          <Header />
+        </div>
+        <div>
+          <Form
+            input={input}
+            setInput={setInput}
+            todos={todos}
+            setTodos={setTodos}
+            editTodo={editTodo}
+            setEditTodo={setEditTodo}
+          />
+        </div>
+        <div>
+          <TodosList
+            todos={todos}
+            setTodos={setTodos}
+            setEditTodo={setEditTodo}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
